408244e759856986588540ddc454e541037c1d9a
PaulLittle <S00156762@mail.itsligo.ie>
2015-10-25 13:15:54 +0000

    first attemt

---
+++ myExpenses/Expense.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myExpenses
+{
+    
+
+    class Expense
+    {
+        private string _category;
+        private double _cost;
+        private DateTime _incurredDate;
+        
+
+        public string Category { get { return _category; } set { _category = value; } }
+        public double Cost
+        {
+            get { return _cost; }
+            set
+            {
+                if (value > 0 || value <= 100)
+                {
+                    _cost = value;
+                }
+                else
+                {
+                    _cost = 0.01;
+                }
+            }
+        }
+
+        public DateTime IncurredDate { get { return _incurredDate; } set { _incurredDate = value; } }
+
+            public override string ToString()
+        {
+            return string.Format("{0}: €{1:f2} on {2}", Category, Cost, IncurredDate.Date);
+        }
+    }  // end class
+}  // end ns
---
+++ myExpenses/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace myExpenses
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        ArrayList expense = new ArrayList();
+
+        string[] categories = new string[] { "Travel", "Entertainment", "Office" };
+        //double totalCost, totCost;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {           
+            populateExpense();
+        }
+
+        private void populateExpense()
+        {
+            DateTime start;
+            Random gen;
+            int range;
+            generateDate(out start, out gen, out range);
+
+            Random rng = new Random(Environment.TickCount);
+            expense.Add(new Expense()
+            {
+                Category = categories[rng.Next(categories.Length)],
+                Cost = 0.01 + rng.NextDouble() * 100.00,
+                IncurredDate = start.AddDays(gen.Next(range))
+            });
+
+            updateListbox();
+            updateTextbox();
+        }
+
+        private void generateDate(out DateTime start, out Random gen, out int range)
+        {
+            start = new DateTime(2015, 9, 23);
+            gen = new Random();
+            range = (DateTime.Today - start).Days;
+        }
+
+        private void updateListbox()
+        {
+            lbxExpenses.Items.Clear();
+            for (int i = 0; i < expense.Count; i++)
+            {
+                lbxExpenses.Items.Add(expense[i]);
+            }
+        }
+
+        private void updateTextbox()
+        {
+            
+           // totCost += tc;
+            tbkTotalExpense.Text = ("Total is: €{0}")/*,Cost*/;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            populateExpense();
+            updateListbox();
+        }
+
+        
+    }
+}
